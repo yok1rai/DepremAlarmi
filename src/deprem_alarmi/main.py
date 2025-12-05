47019b7f10a6b6421790305d0e2aa19a674ffd33
unknown <yok1rai@users.noreply.github.com>
2025-12-05 19:48:13 +0300

    Ana dosya güncellendi

--- src/deprem_alarmi/main.py
+++ src/deprem_alarmi/main.py
@@ -1,9 +1,14 @@
 from fetcher import fetch_earthquakes
 from processor import parse_latest_quake
-from alarm.sound import should_alarm
+from alarm.alarm import handle
+from alarm.sound import load
 from time import sleep as sl
 
+load("alarm", r"assets\sounds\anons.wav", channel_id=0)
+
 last_quake_id = None
+
+print("Deprem alarmı başlatıldı..\n")
 
 while True:
     raw = fetch_earthquakes()
@@ -11,26 +16,26 @@ while True:
 
     if not quake:
         print("Deprem verisi alınamadı")
+        handle(None)
         sl(5)
         continue
 
-    if quake and quake["id"] != last_quake_id:
-        if should_alarm(quake):
-            print("Alarm", quake["place"], quake["magnitude"])
-            last_quake_id = quake["id"]
-        else:
-            print(
-                "Yeni deprem ama alarm eşiği altında",
-                quake["place"], quake["magnitude"])
+    if quake["id"] != last_quake_id:
+        print(
+            "Yeni deprem",
+            quake["place"],
+            quake["magnitude"]
+        )
 
+        handle(quake)
         last_quake_id = quake["id"]
 
     else:
         print(
             "Hala aynı deprem",
             quake["place"],
-            quake["magnitude"])
-
+            quake["magnitude"]
+        )
 
     sl(5)
 
