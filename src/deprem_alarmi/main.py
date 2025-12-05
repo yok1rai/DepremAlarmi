273cc610a16210e4af9fae4a3da6022b20ea7c40
unknown <yok1rai@users.noreply.github.com>
2025-12-05 21:37:16 +0300

    Database'e kaydetme özelliği eklendi

--- src/deprem_alarmi/main.py
+++ src/deprem_alarmi/main.py
@@ -1,16 +1,22 @@
 from pathlib import Path
+from time import sleep as sl
+
 from fetcher import fetch_earthquakes
 from processor import parse_latest_quake
 from alarm.alarm import handle
 from alarm.sound import load
-from time import sleep as sl
+from storage.sqlite import init_db, save_quake, quake_exists
+
+# --- DB ---
+init_db()
+
+# --- ses ---
 
 BASE_DIR = Path(__file__).resolve().parents[2]
 sound_path = BASE_DIR / "assets" / "sounds" / "anons.wav"
-
 load("alarm", sound_path, channel_id=0)
 
-last_quake_id = None
+# --- olay ---
 
 print("Deprem alarmı başlatıldı..\n")
 
@@ -20,23 +26,22 @@ while True:
 
     if not quake:
         print("Deprem verisi alınamadı")
-        handle(None)
         sl(5)
         continue
 
-    if quake["id"] != last_quake_id:
+    if not quake_exists(quake["id"]):
         print(
             "Yeni deprem",
             quake["place"],
             quake["magnitude"]
         )
 
+        save_quake(quake)
         handle(quake)
-        last_quake_id = quake["id"]
 
     else:
         print(
-            "Hala aynı deprem",
+            "Aynı deprem",
             quake["place"],
             quake["magnitude"]
         )
